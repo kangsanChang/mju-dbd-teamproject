99a600cf35951c86d27c6beb4672122551f9c83c
leesk <jack6937@naver.com>
2018-12-01 18:55:26 +0900

    index, layout update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '리승균동무' });
+  res.render('index', { title: 'DB Company' });
 });
 
 module.exports = router;
