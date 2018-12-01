466f7d4ee7a96890cab33485c0ddf7908fcc0d23
leesk <jack6937@naver.com>
2018-12-01 16:00:59 +0900

    ADD nodemon module

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '리승균동무' });
 });
 
 module.exports = router;
